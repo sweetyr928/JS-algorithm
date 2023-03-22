510420878a78fd30921799869aa4098c4ba45330
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2023-03-22 15:07:14 +0900

    [level 3] Title: 여행경로, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43164. 여행경로/여행경로.js
@@ -0,0 +1,20 @@
+const solution = (tickets) => {
+  let answer = [];
+  const DFS = (airport, tickets, path) => {
+    let newPath = [...path, airport];
+    if(tickets.length === 0){
+      answer.push(newPath);
+    }
+    else{
+      tickets.map((ticket, idx) => {
+        if(ticket[0] === airport){
+          let copiedTickets = [...tickets];
+          const [[from, to]] = copiedTickets.splice(idx, 1);
+          DFS(to, copiedTickets, newPath)
+        }
+      });
+    }
+  };
+  DFS("ICN", tickets, []);
+  return answer.sort()[0];
+}
