7d76a8508ff646cf2511249fad37aeaae2e4430b
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2023-03-23 12:03:26 +0900

    [level 1] Title: 체육복, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42862. 체육복/체육복.js
@@ -0,0 +1,17 @@
+function solution(n, lost, reserve) {
+    let count = n - lost.length;
+    lost = lost.sort();
+    
+    for(let i=0;i<lost.length;i++){
+        if(!reserve.length) break;
+        if(reserve.includes(lost[i])) { 
+            reserve.splice(reserve.indexOf(lost[i]),1); count++; 
+        }else if(reserve.includes(lost[i] - 1) && !lost.includes(lost[i] - 1)) {
+            reserve.splice(reserve.indexOf(lost[i] - 1),1); count++;
+        }else if(reserve.includes(lost[i] + 1) && !lost.includes(lost[i] + 1)) {
+            reserve.splice(reserve.indexOf(lost[i] + 1),1); count++;
+        }
+    }
+    
+    return count;
+}
