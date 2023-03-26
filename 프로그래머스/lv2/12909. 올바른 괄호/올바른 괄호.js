68934584c96fa9d60dcfafb4dfaa35f3888a02cc
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2023-03-26 10:43:01 +0900

    [level 2] Title: 올바른 괄호, Time: 8.39 ms, Memory: 37.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,11 @@
+function solution(s){
+    let stack = [];
+    if (s[0] === ")") return false;
+    
+    for(let i=0;i<s.length;i++){
+        if(s[i] === "(") stack.push(s[i]);
+        else if(s[i] === ")") stack.pop();
+    }
+    
+    return stack.length === 0 ? true:false;
+}
