11622c1411a5be3738e4bf759fba750a64cc4778
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2023-03-27 14:27:28 +0900

    [level 1] Title: 모의고사, Time: 3.52 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.js
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.js
@@ -1,24 +1,21 @@
 function solution(answers) {
-
     let answer = [];
+    let one = [1,2,3,4,5];
+    let two = [2,1,2,3,2,4,2,5];
+    let three = [3,3,1,1,2,2,4,4,5,5];
+    let cor = [0,0,0];
 
-    const one = [1, 2, 3, 4, 5];
-    const two = [2, 1, 2, 3, 2, 4, 2, 5];
-    const three = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
-
-    let score = [0, 0, 0];
-
-    for (let i = 0; i < answers.length; i++) {
-        if (answers[i] === one[i % one.length]) score[0]++;
-        if (answers[i] === two[i % two.length]) score[1]++;
-        if (answers[i] === three[i % three.length]) score[2]++;
+    for(let i=0;i<answers.length;i++){
+        if(answers[i] === one[i%one.length]) cor[0]++;
+        if(answers[i] === two[i%two.length]) cor[1]++;
+        if(answers[i] === three[i%three.length]) cor[2]++;
     }
-
-    const max = Math.max(...score);
-
-    for (let i = 0; i < score.length; i++) {
-        if (max === score[i]) answer.push(i + 1);
+    
+    let max = Math.max(...cor);
+  
+    for(let i=0;i<3;i++){
+      if(cor[i] === max) answer.push(i+1);
     }
-
+    
     return answer;
 }
