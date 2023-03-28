336d236917b67e6c317097b0222e38bfcac45825
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2023-03-28 12:58:36 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -0,0 +1,25 @@
+function solution(s) {
+    let cnt = 0;
+    
+    isRight(s) ? cnt++ : null;
+    let newS = s.slice();
+    
+    for(let i=1;i<s.length;i++){
+        newS = newS.slice(1) + newS[0];
+        isRight(newS) ? cnt++ : null;
+    }
+    
+    return cnt;
+}
+
+function isRight(str){
+    let stack = [];
+    if(str[0] === ")" || str[0] === "]" || str[0] === "}") return false;
+    
+    for(let i=0;i<str.length;i++){
+        if(str[i] === "(" || str[i] === "[" || str[i] === "{") stack.push(str[i]);
+        else if(str[i]===")" && stack[stack.length-1]==="(" || str[i]==="}" && stack[stack.length-1]==="{" || str[i]==="]" && stack[stack.length-1]==="[") stack.pop();
+    }
+    
+    return stack.length === 0 ? true : false;
+}
