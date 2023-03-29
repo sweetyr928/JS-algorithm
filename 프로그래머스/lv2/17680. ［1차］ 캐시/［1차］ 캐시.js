c76e4f922649afeee5ef59c46b312e43f2d15231
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2023-03-29 10:49:29 +0900

    [level 2] Title: [1차] 캐시, Time: 1.92 ms, Memory: 35.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.js
@@ -0,0 +1,25 @@
+function solution(cacheSize, cities) {
+    if(cacheSize === 0) return cities.length * 5;
+    cities = cities.map((el) => el.toLowerCase());
+    let answer = 0;
+    let cache = [];
+    
+    for(let i=0;i<cities.length;i++){        
+        if(!cache.includes(cities[i]) && cache.length < cacheSize) {
+            cache.push(cities[i]);
+            answer += 5;
+        }else if(!cache.includes(cities[i]) && cache.length === cacheSize){
+            cache.push(cities[i]);
+            cache.shift();
+            answer += 5;
+        }
+        else{
+            let temp = cache.filter((el) => el !== cities[i]);
+            temp.push(cities[i]);
+            cache = temp;
+            answer++;
+        }
+    }
+    
+    return answer;
+}
