eca89ab91da61a9be6df34f73d53a981c8fbfe9b
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2023-03-30 20:21:14 +0900

    [level 2] Title: 튜플, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/64065. 튜플/튜플.js
@@ -0,0 +1,28 @@
+function solution(s) {
+    let answer = [], newS = [], i= 2, tempS = "", temp = [];
+  
+    while(i < s.length-1){ 
+      if(!isNaN(s[i])) tempS += s[i];
+      if(s[i] === "," && s[i+1] !== "{"){
+        temp.push(+tempS);
+        tempS = "";
+      }
+      if(s[i] === "}"){
+        temp.push(+tempS);
+        newS.push(temp);
+        temp = [];
+        tempS = "";
+      }
+      i++;
+    }
+  
+    newS.sort((a,b) => a.length - b.length);
+  
+    for(let i=0;i<newS.length;i++){
+          for(let j=0;j<newS[i].length;j++){
+            if(!answer.includes(newS[i][j])) answer.push(newS[i][j]);
+          }
+    }
+    
+    return answer;
+}
