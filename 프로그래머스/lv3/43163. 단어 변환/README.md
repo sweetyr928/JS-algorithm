76d3cf367e4ddb6a8249a8bebeb5431f904b0c16
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2023-03-31 16:09:27 +0900

    [level 3] Title: 단어 변환, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv3/43163. 단어 변환/README.md
+++ 프로그래머스/lv3/43163. 단어 변환/README.md
@@ -1,10 +1,10 @@
 # [level 3] 단어 변환 - 43163 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.07 ms
+메모리: 33.5 MB, 시간: 0.06 ms
 
 ### 구분
 
--- 프로그래머스/lv3/43163. 단어 변환/단어 변환.js
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.js
@@ -1,37 +1,27 @@
 function solution(begin, target, words) {
-    let answer = 0;
     let visited = [];
     let queue = [];
-
-    // 예외처리
-    if(!words.includes(target)) return 0;
-    // [시작단어, 변경횟수]
-    queue.push([begin,answer]); 
-
-    while(queue) {
-        let [v, cnt] = queue.shift();
-
-        if (v === target) { 
-            return cnt;
-        }
-
-        words.forEach(word => {
-            // 다른 단어의 갯수
-            let notEqual = 0; 
-            // 방문했던 단어면 pass
-            if(visited.includes(word)) return; 
-
-            for (let i=0; i<word.length; i++) {
-                if (word[i] !== v[i]) notEqual++;
-            }
-            //만약 다른게 1개라면 queue에 [단어, 횟수] 형태로 추가
-            if (notEqual === 1) { 
-                queue.push([word, ++cnt]); 
-              	//  hit -> hot이 되었을 때 [hot,1] 형태로 queue에 추가됨;
-                visited.push(word); //방문처리를 해준다.
-            }
-        });
-    }
     
-    return answer;
+    if(!words.includes(target)) return 0;
+    
+    queue.push([begin,0]);
+    
+    while(queue.length){
+        let [word, cnt] = queue.shift();
+        if(word === target) return cnt;
+        
+        words.map((el) => {
+                let diff = 0;
+                
+                for(let i=0;i<el.length;i++){
+                    if(el[i] != word[i]) diff++;
+                }
+                
+                if(diff === 1){
+                    queue.push([el,cnt+1]);
+                    visited.push(el);
+                } 
+            })
+    }
+ 
 }
