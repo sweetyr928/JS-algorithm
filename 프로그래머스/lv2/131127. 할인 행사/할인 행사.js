d96a9a6eeb1119b3ae3feb77635bce8dcc602b63
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2023-04-06 00:26:34 +0900

    [level 2] Title: 할인 행사, Time: 0.10 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/lv2/131127. 할인 행사/할인 행사.js
+++ 프로그래머스/lv2/131127. 할인 행사/할인 행사.js
@@ -1,17 +1,19 @@
 function solution(want, number, discount) {
-  var answer = 0;
+  let answer = 0;
   
-  const isMatch = (arr) => { // 할인 품목과 원하는 제품이 일치하는지 확인하는 함수
-      var map = new Map();   // 매번 map을 초기화 해준다.
-      arr.forEach(v=>map.set(v,(map.get(v)||0)+1)); // 할인 품목들을 map에 셋팅한다.
+  // 할인 품목과 원하는 제품이 일치하는지 확인하는 함수
+  const isMatch = (arr) => { 
+      let map = new Map();   // 매번 map을 초기화
+      arr.forEach(el => map.set(el,(map.get(el)||0)+1)); // 할인 품목들을 map에 셋팅
       for(let i = 0; i < want.length; i++){
-          if(map.get(want[i])!==number[i]) return false;
-      }             // 원하는 품목의 수량과 할인 품목이 일치하지 않으면 false
-      return true;  // 일치하면 true를 리턴한다
+          // 원하는 품목의 수량과 할인 품목이 일치하지 않으면 false
+          if(map.get(want[i]) !== number[i]) return false;
+      }             
+      return true;  
   }
   
   for(let j = 0; j <= discount.length - 10; j++){
-      var arr = discount.slice(j, j+10); // 10일동안 할인이 되므로 10개씩 배열을 잘라준다.
+      let arr = discount.slice(j, j+10);
       if(isMatch(arr)){
           answer++;
       } 
