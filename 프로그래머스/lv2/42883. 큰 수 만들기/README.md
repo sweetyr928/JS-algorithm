f3df7d2474152f0f1f4aef115fd32ca3ae846da2
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2023-04-11 13:59:07 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42883. 큰 수 만들기/README.md
+++ 프로그래머스/lv2/42883. 큰 수 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 큰 수 만들기 - 42883 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42883#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42883) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.06 ms
+메모리: 33.4 MB, 시간: 0.07 ms
 
 ### 구분
 
--- 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.js
+++ 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.js
@@ -1,17 +1,16 @@
 function solution(number, k) {
     let stack = [];
-	// 원본 배열도 스택으로 만들기
-    let arr = number.split('').reverse();
-
+    let arr = number.split("").reverse();
+    
     while(arr.length && k > 0){
-        stack.push(arr.pop())
-        while(stack[stack.length-1] < arr[arr.length-1] && k>0) {
+        stack.push(arr.pop());
+        while(stack[stack.length-1] < arr[arr.length-1] && k > 0){
             stack.pop();
-            k = k-1;
+            k--;
         }
     }
-	// 원본 배열에서 다 스택으로 꺼냈는데도 k가 0 이상이라면 스택 뒤에서 자르기
-    if(k !== 0) stack = stack.slice(0, -k) 
-
-    return stack.join('') + arr.reverse().join('')
+    
+    if(k > 0) stack = stack.slice(0,-k);
+    
+    return stack.join("") + arr.reverse().join("");
 }
