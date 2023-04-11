b45ae31d3f27a17560e17cd7fd527a22d3f992d2
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2023-04-11 13:13:00 +0900

    [level 2] Title: 땅따먹기, Time: 24.63 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.js
@@ -0,0 +1,29 @@
+function solution(land) {
+  let answer = [];
+  let len = land.length;
+  for (let i = 1; i < len; i++) {
+    land[i][0] += Math.max(
+      land[i - 1][1],
+      land[i - 1][2],
+      land[i - 1][3]
+    );
+    land[i][1] += Math.max(
+      land[i - 1][0],
+      land[i - 1][2],
+      land[i - 1][3]
+    );
+    land[i][2] += Math.max(
+      land[i - 1][0],
+      land[i - 1][1],
+      land[i - 1][3]
+    );
+    land[i][3] += Math.max(
+      land[i - 1][0],
+      land[i - 1][1],
+      land[i - 1][2]
+    );
+  }
+  answer = land[land.length - 1];
+
+  return Math.max(...answer);
+}
