dffa5c812399c55777bb123299f0aa1280a3714d
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2023-04-13 23:37:10 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 3.43 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/lv2/86971. 전력망을 둘로 나누기/README.md
+++ 프로그래머스/lv2/86971. 전력망을 둘로 나누기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전력망을 둘로 나누기 - 86971 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86971) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86971?language=javascript) 
 
 ### 성능 요약
 
-메모리: 38.1 MB, 시간: 4.74 ms
+메모리: 38.1 MB, 시간: 3.43 ms
 
 ### 구분
 
@@ -12,7 +12,7 @@
 
 ### 채점결과
 
-<br/>정확성: 100.0<br/>합계: 100.0 / 100.0
+Empty
 
 ### 문제 설명
 
--- 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
+++ 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
@@ -1,36 +1,42 @@
 function solution(n, wires) {
-    const links = {}; 
-    wires.map(w => {
-        const [a, b] = w;
-        if(!links[a]) links[a] = [];
-        if(!links[b]) links[b] = [];
-        links[a].push(b);
-        links[b].push(a);
-    })
+    // wires 배열을 탐색하며 노드간 연결관계 객체로 저장
+    // 루트 노드,제외할 특정 노드를 파라미터로 갖고 노드 별 간선 갯수 리턴하는 함수 작성
+    // wires 배열 탐색하며 한 개의 연결 끊고 위에서 만든 함수에 각각 루트노드, 제외노드로 넣어 결과의 차가 가장 적은 경우 리턴
+    let answer = 0;
+    let tree = {};
+    wires.map((el) => {
+        if(!(el[0] in tree)) tree[el[0]] = [];
+        if(!(el[1] in tree)) tree[el[1]] = [];
+        tree[el[0]].push(el[1]);
+        tree[el[1]].push(el[0]);
+    });
     
-    const searchTree = (root, exception) => {
+    const edge = (root, except) => {
         let count = 0;
-        const queue = [root];
-        const visited = [];
+        let queue = [root];
+        let visited = [];
         visited[root] = true;
-        while(queue.length){
-            const cur = queue.pop();
-            links[cur].map(next => {
-                if(next !== exception && !visited[next]){
-                    visited[next] = true;
-                    queue.push(next);
+        
+        while(queue.length > 0){
+            let cur = queue.pop();
+            tree[cur].map((el) => {
+                if(el!==except && !visited[el]) {
+                    queue.push(el);
+                    visited[el] = true;
                 }
             })
             count++;
         }
+        
         return count;
     }
     
-    let answer = 100; 
-    wires.map((w, i) => { 
-        const [a, b] = w;
-        const dif = Math.abs(searchTree(a,b) - searchTree(b,a));
-        answer = answer > dif ? dif : answer;
+    answer = 100;
+    
+    wires.map((el) => {
+        let temp = Math.abs(edge(el[0],el[1]) - edge(el[1],el[0]));
+        if(answer > temp) answer = temp;
     })
+    
     return answer;
 }
