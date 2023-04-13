6d0abb6bf598ad1f5c78c9da8e78879fd9c881a6
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2023-04-13 21:07:12 +0900

    [level 1] Title: 최소직사각형, Time: 24.81 ms, Memory: 39.3 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
@@ -1,13 +1,19 @@
 function solution(sizes) {
-    let width = 0; let height = 0;
-    
+    // 1. 가로 길이 < 세로 길이 : 두 값 바꿔주기
+    // 2. 다 바꿨으면 가로길이 기준 정렬 후 마지막 값 * 세로길이 기준 정렬 후 마지막 값
     for(let i=0;i<sizes.length;i++){
+        let temp = 0;
         if(sizes[i][0] < sizes[i][1]){
-            [sizes[i][0],sizes[i][1]] = [sizes[i][1],sizes[i][0]];
+            temp = sizes[i][0];
+            sizes[i][0] = sizes[i][1];
+            sizes[i][1] = temp;
         }
-        if(sizes[i][0] > width) width = sizes[i][0];
-        if(sizes[i][1] > height) height = sizes[i][1];
     }
     
-    return width * height;
+    let temp_width = sizes.slice().sort((a,b) => b[0] - a[0]);
+    let temp_height = sizes.slice().sort((a,b) => b[1] - a[1]);
+    
+    console.log(temp_width,temp_height)
+    
+    return temp_width[0][0] * temp_height[0][1];
 }
