5fa3fe8b40307115cc72c43a126527d49e6ab284
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2023-04-13 23:08:31 +0900

    [level 2] Title: 피로도, Time: 0.24 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/피로도.js
+++ 프로그래머스/lv2/87946. 피로도/피로도.js
@@ -1,20 +1,24 @@
 function solution(k, dungeons) {
+    // 방문한 던전을 제외한 나머지 배열, k를 파라미터로 갖는 dfs
+    // 던전을 방문할 때마다 방문 깊이를 저장하고 그 저장한 방문 깊이 중 최댓값을 반환하면 됨
     let answer = [];
-    let visited = new Array(dungeons.length).fill(0);
+    dungeons.sort((a,b) => b[0]-a[0]);
     
-    const dfs = (count,newK) => {
-        answer.push(count);
-        
-        for(let i=0;i<dungeons.length;i++){
-            if(newK >= dungeons[i][0] && !visited[i]){
-                visited[i] = 1;
-                dfs(count+1, newK-dungeons[i][1]);
-                visited[i] = 0;
+    const dfs = (newK, newD) => {
+        answer.push(dungeons.length - newD.length);
+        if(newK > 0){
+            for(let i=0;i<newD.length;i++){
+                if(newK >= newD[i][0]){
+                    let tempK = newK - newD[i][1];
+                    let tempD = newD.slice();
+                    tempD.splice(i,1);
+                    dfs(tempK,tempD);
+                } 
             }
         }
     }
     
-    dfs(0,k);
+    dfs(k, dungeons);
     
     return Math.max(...answer);
 }
