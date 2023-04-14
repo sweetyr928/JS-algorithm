db53498f18ca88e462185affa71ba6b9cafadc77
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2023-04-14 17:30:55 +0900

    [level 3] Title: 여행경로, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv3/43164. 여행경로/여행경로.js
+++ 프로그래머스/lv3/43164. 여행경로/여행경로.js
@@ -1,23 +1,27 @@
 function solution(tickets) {
+    // 최소 경로니까 bfs. 파라미터 : 현재 있는 공항, 경로, 남은 티켓
+    // 모든 항공권을 사용하는 경우를 모아 알파벳 순으로 정렬
     let answer = [];
+    let visited = new Array(tickets.length).fill(0);
     
-    const dfs = (target, tickets, path) => {    
-        let newPath = [...path, target];
-        if(tickets.length === 0){
-            answer.push(newPath);
-        }
-        else{
-            tickets.map((el,idx) => {
-                if(el[0] === target){
-                    let newTickets = tickets.slice();
-                    newTickets.splice(idx,1); 
-                    dfs(el[1], newTickets, newPath);
+    const bfs = (airport, visit) => {
+        if(visit.length === tickets.length + 1){
+            answer.push(visit);
+            return;
+        }else{
+            for(let i=0;i<tickets.length;i++){
+                if(tickets[i][0] === airport && !visited[i]){
+                    visited[i] = 1;
+                    let newV = visit.slice();
+                    newV.push(tickets[i][1]);
+                    bfs(tickets[i][1], newV);
+                    visited[i] = 0;
                 }
-            })
+            }
         }
     }
     
-    dfs("ICN", tickets, []);
+    bfs("ICN", ["ICN"]);
     
     return answer.sort()[0];
 }
