8fe7db917b18ccb8f586fa45a0868d32829b9efd
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2023-04-14 13:30:25 +0900

    [level 2] Title: 타겟 넘버, Time: 2.53 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,16 +1,25 @@
 function solution(numbers, target) {
+    // dfs 문제. 파라미터는 총 합, 남은 배열
+    // 모든 경우 중 결과가 target과 같은 경우만 세기
     let answer = 0;
     
-    const dfs = (i,sum) => {
-        if(i === numbers.length){
-            if(sum === target) answer++;
+    const dfs = (total, newNum) => {
+        // 엣지 케이스
+        if(!newNum.length){
+            if(total === target){
+                answer++;
+                return;
+            }
         }else{
-            dfs(i + 1, sum + numbers[i]);
-            dfs(i + 1, sum - numbers[i]);
+            let cur = newNum[0];
+            let newNumbers = newNum.slice();
+            newNumbers.shift();
+            dfs(total + cur, newNumbers);
+            dfs(total - cur, newNumbers);
         }
     }
     
-    dfs(0,0);
+    dfs(0, numbers);
     
     return answer;
 }
