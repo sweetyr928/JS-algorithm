1a11020cd6bb84a8ab5a8e78d873538ed7eefd0a
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2023-04-14 17:08:58 +0900

    [level 3] Title: 단어 변환, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv3/43163. 단어 변환/단어 변환.js
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.js
@@ -1,27 +1,30 @@
 function solution(begin, target, words) {
-    let visited = [];
-    let queue = [];
-    
-    if(!words.includes(target)) return 0;
-    
-    queue.push([begin,0]);
+    // 최소 단계니까 bfs
+    // 단어 visited 배열 필수
+    // 현재 단어와 한 개의 알파벳만 다른 경우 queue에 push
+    // queue [단어, cnt]
+    let answer = 0;
+    let visited = new Array(words.length).fill(0);
+    let queue = [[begin,0]];
     
     while(queue.length){
-        let [word, cnt] = queue.shift();
-        if(word === target) return cnt;
-        
-        words.map((el) => {
+        let cur = queue.shift();
+        if(cur[0] === target){
+            answer = cur[1];
+            break;
+        }else{
+            for(let i=0;i<words.length;i++){
                 let diff = 0;
-                
-                for(let i=0;i<el.length;i++){
-                    if(el[i] != word[i]) diff++;
+                for(let j=0;j<words[i].length;j++){
+                    if(words[i][j] !== cur[0][j]) diff++;
                 }
-                
-                if(diff === 1){
-                    queue.push([el,cnt+1]);
-                    visited.push(el);
-                } 
-            })
+                if(diff === 1 && !visited[i]) {
+                    queue.push([words[i],cur[1]+1]);
+                    visited[i] = 1;
+                }
+            }
+        }
     }
- 
+    
+    return answer;
 }
