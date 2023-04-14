dd4c75c1fe4ac45244da1ef82d38f1bb1ce83e8b
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2023-04-14 11:03:32 +0900

    [level 2] Title: 기능개발, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.js
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.js
@@ -1,21 +1,22 @@
 function solution(progresses, speeds) {
-    // 큐의 개념 이용 => 선입선출(먼저 배포 되어야하는 작업이 먼저 실행됨)
+    // 모든 작업의 소요 시간 구하기
+    // 큐를 사용해 작업기간 max 값(임시로 첫번째 값으로 둠)보다 큰 값이 나오면 그 전 작업 배포
     let answer = [];
-    // 각 작업 별 소요되는 작업기간 구하기
-    let days = progresses.map((el,idx) => Math.ceil((100 - el) / speeds[idx]));
-    // 각 배포마다 몇 개의 기능이 배포되는지 확인
-    // 뒤에 있는 기능이 완료됐더도 앞에 있는 기능이 배포될 때까지 기다렸다가 함께 배포되어야 함
-    let max = days[0];
-    let count = 1;
-    for(let i=1;i<days.length;i++){
-      if(max < days[i]){
-        answer.push(count);
-        max = days[i];
-        count = 1;
-      }else count++;
-    }
-    // 마지막 배포 날짜의 배포되는 기능 수까지 push
-    answer.push(count);
+    let time = progresses.map((el,idx) => Math.ceil((100-el)/speeds[idx]));
+    let max = time[0];
+    let cnt = 0;
+    
+    time.map((el) => {
+        if(max >= el){
+            cnt++;
+        }else{
+            answer.push(cnt);
+            cnt = 1;
+            max = el;
+        }
+    })
+    
+    answer.push(cnt);
     
     return answer;
 }
