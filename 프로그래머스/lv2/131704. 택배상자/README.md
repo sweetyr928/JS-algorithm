2fb1c353e2ba09e46eb0b8d0686efa7ecc8c5e86
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2023-05-10 13:50:59 +0900

    [level 2] Title: 택배상자, Time: 56.05 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/lv2/131704. 택배상자/README.md
+++ 프로그래머스/lv2/131704. 택배상자/README.md
@@ -1,10 +1,10 @@
 # [level 2] 택배상자 - 131704 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131704#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131704) 
 
 ### 성능 요약
 
-메모리: 80.2 MB, 시간: 65.38 ms
+메모리: 80 MB, 시간: 56.05 ms
 
 ### 구분
 
--- 프로그래머스/lv2/131704. 택배상자/택배상자.js
+++ 프로그래머스/lv2/131704. 택배상자/택배상자.js
@@ -1,12 +1,12 @@
 function solution(order) {
     let answer = 0;
-    let main = order.reverse();
-    let left = order.map((el,idx) => idx+1).reverse(); // 택배 숫자 순
-    let sub = [];
+    let main = order.reverse(); // pop이 shift 보다 시간 덜 소요 되므로 순서 역순
+    let left = order.map((el,idx) => idx+1).reverse(); // 택배 숫자 역순
+    let sub = []; // 보조 컨테이너(stack)
     
     while(true){   
-        if(!left.length && !sub.length) break;
-        // console.log(order,left,sub,answer)
+        // 택배를 모조리 보조 컨테이너 옮긴 후 보조 컨테이너에 택배가 없을 때 break
+        if(!left.length && !sub.length) break; 
         
         if(left.length && main[main.length-1] === left[left.length-1]){
             answer++;
@@ -18,9 +18,12 @@ function solution(order) {
             answer++;
             order.pop();
         }else{
+            // 보조 컨테이너에 옮기거나 트럭에 넣을 남은 택배도 없고, 
+            // 다음 순서 택배가 보조 컨테이너에서 꺼낼 수 있는 택배와 다른 경우
+            // 더 이상 진행할 수 없으므로 break
             if(!left.length && main[main.length-1] !== sub[sub.length-1]) break;
             else{
-                sub.push(left[left.length-1]);
+                sub.push(left[left.length-1]); //메인 컨테이너에 택배 있다면 보조컨테이너에 옮기기
                 left.pop();
             }
 
