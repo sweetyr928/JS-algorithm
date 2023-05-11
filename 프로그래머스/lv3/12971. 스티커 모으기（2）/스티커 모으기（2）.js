d55be944bc37dda86d7743ae4fa2019421dcc8f2
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2023-05-11 15:56:22 +0900

    [level 3] Title: 스티커 모으기(2), Time: 7.02 ms, Memory: 39.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/12971. 스티커 모으기（2）/스티커 모으기（2）.js
@@ -0,0 +1,15 @@
+function solution(sticker) {
+  const len = sticker.length + 2;
+  const dp1 = new Array(len).fill(0);
+  const dp2 = new Array(len).fill(0);
+  
+  if(sticker.length === 1) return sticker[0];
+  
+  for(let i = 2; i < len-1; i++)
+    dp1[i] = Math.max(dp1[i-1], dp1[i-2] + sticker[i-2]);
+  
+  for(let i = 3; i < len; i++) 
+    dp2[i] = Math.max(dp2[i-1], dp2[i-2] + sticker[i-2]);
+  
+  return Math.max(dp1[len-2], dp2[len-1]);
+}
