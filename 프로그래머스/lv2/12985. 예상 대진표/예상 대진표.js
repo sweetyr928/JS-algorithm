d4c29086a3b78b30c49765fa6e651bf46da73b90
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2023-05-22 18:48:33 +0900

    [level 2] Title: 예상 대진표, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.js
@@ -1,11 +1,11 @@
 function solution(n,a,b) {
-    let answer = 0;
-    
+    let cnt = 0;
+    //  a와 b가 같아지면 같은 라운드에 있다는 말과 같다.
     while(a !== b) {
         a = Math.ceil(a/2);
         b = Math.ceil(b/2);
-        answer++;
+        cnt++;
     }
 
-    return answer;
+    return cnt;
 }
