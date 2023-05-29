b2e1fb540faf5d46b56303f7420ba7fefd775a10
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2023-05-29 22:56:50 +0900

    [level 2] Title: 할인 행사, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/131127. 할인 행사/README.md
+++ 프로그래머스/lv2/131127. 할인 행사/README.md
@@ -1,10 +1,10 @@
 # [level 2] 할인 행사 - 131127 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127) 
 
 ### 성능 요약
 
-메모리: 33 MB, 시간: 0.10 ms
+메모리: 33.4 MB, 시간: 0.13 ms
 
 ### 구분
 
@@ -12,7 +12,7 @@
 
 ### 채점결과
 
-<br/>정확성: 100.0<br/>합계: 100.0 / 100.0
+Empty
 
 ### 문제 설명
 
--- 프로그래머스/lv2/131127. 할인 행사/할인 행사.js
+++ 프로그래머스/lv2/131127. 할인 행사/할인 행사.js
@@ -1,22 +1,19 @@
 function solution(want, number, discount) {
-  let answer = 0;
-  
-  // 할인 품목과 원하는 제품이 일치하는지 확인하는 함수
-  const isMatch = (arr) => { 
-      let map = new Map();   // 매번 map을 초기화
-      arr.forEach(el => map.set(el,(map.get(el)||0)+1)); // 할인 품목들을 map에 셋팅
-      for(let i = 0; i < want.length; i++){
-          // 원하는 품목의 수량과 할인 품목이 일치하지 않으면 false
-          if(map.get(want[i]) !== number[i]) return false;
-      }             
-      return true;  
-  }
-  
-  for(let j = 0; j <= discount.length - 10; j++){
-      let arr = discount.slice(j, j+10);
-      if(isMatch(arr)){
-          answer++;
-      } 
-  }
+    let answer = 0;
+
+    const isRight = (arr) => {
+        let map = new Map();
+        for(let i=0;i<arr.length;i++) map.set(arr[i], (map.get(arr[i]) || 0) + 1);
+        for(let i=0;i<want.length;i++){
+            if(map.get(want[i]) !== number[i]) return false;
+        }
+        return true;
+    }
+    
+    for(let i=0;i<=discount.length-10;i++){
+        let temp = discount.slice(i,i+10);
+        if(isRight(temp)) answer++;
+    }
+    
     return answer;
 }
