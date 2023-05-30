aa22e69497231e4668e58c5439874b2ffaa7b3e4
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2023-05-30 23:56:10 +0900

    [level 3] Title: 최고의 집합, Time: 0.06 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/lv3/12938. 최고의 집합/최고의 집합.js
+++ 프로그래머스/lv3/12938. 최고의 집합/최고의 집합.js
@@ -1,7 +1,8 @@
 function solution(n, s) {
     if(n > s) return [-1];
+    // 집합의 첫 요소를 s/n부터 시작하여 집합 요소 간의 편차 줄이기
     const answer = new Array(n).fill(Math.floor(s / n));
-  
+  	// s/n 나머지를 남은 요소들에게 나눠주기
     for(let i = 0; i < s % n; i ++)
         answer[answer.length - 1 - i]++;
   
