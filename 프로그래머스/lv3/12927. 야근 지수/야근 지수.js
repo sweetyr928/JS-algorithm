eb61eec0ecb4001c6075b1fd9ae438954d1a9ba6
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2023-06-02 17:08:21 +0900

    [level 3] Title: 야근 지수, Time: 28.54 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/lv3/12927. 야근 지수/야근 지수.js
+++ 프로그래머스/lv3/12927. 야근 지수/야근 지수.js
@@ -1,10 +1,12 @@
 function solution(n, works) {
-    if(works.reduce((a,c) => a += c) <= n) return 0;
+    if(works.reduce((a,c)=>a+=c) <= n) return 0;
+    let answer = 0;
+    // 모든 요소의 편차를 줄여주기 위해 정렬
     let sorted = works.sort((a,b) => a-b);
   
     while(n) {
+    	// 가장 큰 값 먼저 줄여줌. 그래야 최종 값이 최대한 줄어들기 때문.
         const max = sorted[works.length-1];
-    
         for(let i = works.length-1; i >= 0; i--) {
             if(sorted[i] >= max) {
                 sorted[i]--;
@@ -13,6 +15,6 @@ function solution(n, works) {
             if(!n) break;
         }
     }
-  
-    return sorted.reduce((a,c) => a + Math.pow(c, 2), 0);
+
+    return works.reduce((a,c) => a + Math.pow(c, 2), 0);
 }
