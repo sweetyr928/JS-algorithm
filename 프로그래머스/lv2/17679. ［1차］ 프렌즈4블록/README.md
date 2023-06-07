760d9e3a9a4e4f12e6f9ca092ab7ebdd39e3d9ee
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2023-06-07 16:36:20 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 5.12 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv2/17679. ［1차］ 프렌즈4블록/README.md
+++ 프로그래머스/lv2/17679. ［1차］ 프렌즈4블록/README.md
@@ -1,10 +1,10 @@
 # [level 2] [1차] 프렌즈4블록 - 17679 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17679#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17679) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 3.14 ms
+메모리: 33.7 MB, 시간: 5.12 ms
 
 ### 구분
 
--- 프로그래머스/lv2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.js
+++ 프로그래머스/lv2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.js
@@ -1,21 +1,21 @@
 function solution(m, n, board) {
     let answer = 0;
-    let newB = [];
+    const newB = [];
     
-    for(let i=0;i<m;i++){
+    for(let i=0;i<board.length;i++){
         let temp = [];
-        for(let j=0;j<n;j++){
+        for(let j=0;j<board[i].length;j++){
             temp.push([board[i][j],0]);
         }
         newB.push(temp);
     }
-
+    
     while(true){
         let cur = 0;
-        // 지워질 블록 체크
+        // 같은 블록 찾기
         for(let i=0;i<m-1;i++){
             for(let j=0;j<n-1;j++){
-                if(newB[i][j].length && newB[i+1][j].length && newB[i][j][0].charCodeAt() >= 65 && newB[i][j][0].charCodeAt() <= 91 && newB[i][j][0] === newB[i][j+1][0] 
+                if(newB[i][j].length && newB[i+1][j].length && newB[i][j][0] === newB[i][j+1][0] 
                    && newB[i][j][0] === newB[i+1][j][0] && newB[i+1][j][0] === newB[i+1][j+1][0]){
                     if(!newB[i][j][1]){
                       newB[i][j][1] = 1;
@@ -39,32 +39,30 @@ function solution(m, n, board) {
         // 윗 블록 내리기
         for(let i=0;i<m;i++){
             for(let j=0;j<n;j++){
-                if(newB[i][j][1] && i > 0) {
-                  newB[i][j] = [...newB[i-1][j]];
-                  newB[i-1][j] = [];
-                }
-                else if(newB[i][j][1] && i === 0){
-                  newB[i][j] = [];
+                if(newB[i][j][1] && i === 0){
+                    newB[i][j] = [];
+                }else if(newB[i][j][1] && i > 0){
+                    newB[i][j] = [...newB[i-1][j]];
+                    newB[i-1][j] = [];
                 }
             }
-
         }
-  
+        // 빈 곳 채우기
         for(let i=m-1;i>0;i--){
             for(let j=0;j<n;j++){
                 if(!newB[i][j].length){
                     for(let k=i-1;k>=0;k--){
-                      if(newB[k][j].length){
-                        newB[i][j] = [...newB[k][j]];
-                        newB[k][j] = [];
-                        break;
-                      }
+                        if(newB[k][j].length){
+                            newB[i][j] = [...newB[k][j]];
+                            newB[k][j] = [];
+                            break;
+                        }
                     }
                 }
             }
         }
         answer += cur;
-      
+        
         if(!cur) break;
     }
     
