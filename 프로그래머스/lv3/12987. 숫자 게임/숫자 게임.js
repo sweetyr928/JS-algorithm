213624b0943f8cea44ccfc44944862cd6b42c1c4
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2023-06-13 10:42:26 +0900

    [level 3] Title: 숫자 게임, Time: 83.11 ms, Memory: 44.6 MB -BaekjoonHub

--- 프로그래머스/lv3/12987. 숫자 게임/숫자 게임.js
+++ 프로그래머스/lv3/12987. 숫자 게임/숫자 게임.js
@@ -1,16 +1,14 @@
 function solution(A, B) {
-    A.sort((a, b) => b - a);
-    B.sort((a, b) => b - a);
-
-    let j = 0; // B를 가리키는 인덱스
-    let ans = 0; // 점수
-
-    for (let i = 0; i < A.length; i++) { // i는 A를 가리키는 인덱스
-        if (A[i] < B[j]) { // B가 더 클 때 
-            ans++; // 점수 증가
-            j++; // B를 가리키는 인덱스 증가
+    let answer = 0, j = 0;
+    A.sort((a,b) => b-a);
+    B.sort((a,b) => b-a);
+    
+    for(let i=0;i<A.length;i++){
+        if(A[i] < B[j]){
+            answer++;
+            j++;
         }
     }
-
-    return ans;
+    
+    return answer;
 }
