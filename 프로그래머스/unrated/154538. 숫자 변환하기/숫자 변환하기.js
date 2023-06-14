8b979d695282166fad477963d3fa54b850238fd6
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2023-06-14 12:28:04 +0900

    [unrated] Title: 숫자 변환하기, Time: 0.97 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/unrated/154538. 숫자 변환하기/숫자 변환하기.js
+++ 프로그래머스/unrated/154538. 숫자 변환하기/숫자 변환하기.js
@@ -1,19 +1,22 @@
 function solution(x, y, n) {
-  if (x === y) return 0;
-  const dp = {};
-  dp[x] = 0;
-  let data = [x];
-  while (data.length) {
-    const newData = [];
-    for (const d of data) {
-      for (const e of [d + n, d * 2, d * 3]) {
-        if (e > y || dp[e]) continue;
-        if (e === y) return dp[d] + 1;
-        dp[e] = dp[d] + 1;
-        newData.push(e);
-      }
+    if(x === y) return 0;
+    const dp = {};
+    dp[x] = 0;
+    let candidate = [x];
+    
+    while(candidate.length){
+        const newCan = [];
+        for (const el of candidate){
+            for(const c of [el + n, el * 2, el *3 ]){
+                if(c > y || dp[c]) continue;
+                if(c === y) return dp[el] + 1;
+                
+                dp[c] = dp[el] + 1;
+                newCan.push(c);
+            }
+        }
+        candidate = newCan;
     }
-    data = newData;
-  }
-  return -1;
+    
+    return -1;
 }
