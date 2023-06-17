b0bc386ea1590a25bff8ba5c69b9b70eaccf8168
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2023-06-17 11:55:51 +0900

    [level 2] Title: 소수 찾기, Time: 0.44 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.js
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.js
@@ -1,35 +1,29 @@
 function solution(numbers) {
-    // 소수 판별 함수 만들기
-    // fixed 요소, fixed 요소 제외한 numbers 배열을 파라미터로 하는 dfs 함수 만들기
-    // 이미 만든 소수라면 pass, 없다면 push -> 다음 단어 붙혀서 다시 dfs 함수로 검사
     let answer = [];
+    numbers = numbers.split("");
     
-    const isRight = (num) => {
-        if(num <= 1) return false;
-        for(let i=2;i<=Math.sqrt(num);i++){
-            if(num % i === 0) return false;
+    const isRight = (n) => {
+        if(n<=1) return false;
+        for(let i=2;i<=Math.sqrt(n);i++){
+            if(n%i === 0) return false;
         }
-    
         return true;
     }
     
-    const dfs = (fixed, newNum) => {
-        if(newNum.length > 0){
-            for(let i=0;i<newNum.length;i++){
-                let newNumbers = newNum.slice();
-                newNumbers.splice(i,1);
-                let newFixed = fixed+newNum[i];
-                if(isRight(+newFixed) && !answer.includes(+newFixed)){
-                    answer.push(+newFixed);  
-                } 
-                dfs(+newFixed, newNumbers);
+    const dfs = (left,fixed) => {
+        if(left.length > 0){
+            for(let i=0;i<left.length;i++){
+                let temp = fixed + left[i];
+                let newLeft = left.slice();
+                newLeft.splice(i,1);
+                if(isRight(+temp) && !answer.includes(+temp))
+                    answer.push(+temp);
+                dfs(newLeft,temp);
             }
         }
-        
     }
     
-    dfs("", numbers.split(""));
+    dfs(numbers,"");
     
     return answer.length;
 }
-
