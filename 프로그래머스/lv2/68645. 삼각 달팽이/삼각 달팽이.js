e19a35e6cf376b13d5d9d377941e23f83bf43d37
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2023-06-20 11:30:07 +0900

    [level 2] Title: 삼각 달팽이, Time: 2759.19 ms, Memory: 112 MB -BaekjoonHub

--- 프로그래머스/lv2/68645. 삼각 달팽이/삼각 달팽이.js
+++ 프로그래머스/lv2/68645. 삼각 달팽이/삼각 달팽이.js
@@ -1,35 +1,33 @@
 function solution(n) {
-  let answer = [];
-  let count = 0;
-  let arr = Array.from({ length: n }, (el, idx) => Array(idx + 1).fill(0));
-  let currentX = -1; // 상하
-  let currentY = 0; // 좌우
-  while (n > 0) { // n의 값이 유효할 때까지 반복(안에 있는 세모들도 결국 같은 모양새이므로)
-    for (let i = 0; i < n; i++) {
-      // 위에서 아래로 내려가는 대각선
-      currentX++;
-      count++;
-      arr[currentX][currentY] = count;
+    let answer = [];
+    let arr = Array.from({length : n}, (_,idx) => new Array(idx+1).fill(0));
+    let x = 0; // 좌우
+    let y = -1; // 상하
+    let cur = 0;
+    
+    while(n > 0){
+        for(let i=0;i<n;i++){ // 위에서 아래
+            y++;
+            cur++;
+            arr[y][x] = cur;
+        }
+        for(let i=0;i<n-1;i++){ // 왼에서 오
+            x++;
+            cur++;
+            arr[y][x] = cur;
+        }
+        for(let i=0;i<n-2;i++){ // 아래에서 위
+            x--;
+            y--;
+            cur++;
+            arr[y][x] = cur;
+        }
+        n -= 3;
     }
-    for (let i = 0; i < n - 1; i++) {
-      // 왼쪽에서 오른쪽으로 가는 선
-      currentY++; // 이미 위의 for문을 통해 currentX 값이 삼각형 가장 밑 변이므로
-      count++;
-      arr[currentX][currentY] = count;
+    
+    for(let i=0;i<arr.length;i++){
+        answer = [...answer,...arr[i]]
     }
-    for (let i = 0; i < n - 2; i++) {
-      // 아래에서 위로 내려가는 대각선
-      currentX--;
-      currentY--;
-      count++;
-      arr[currentX][currentY] = count;
-    }
-    n -= 3; // 매 while 문 총 세 번의 칸 채움이 있음(위아래, 왼오, 아래위)
-  }
-
-  for (let i = 0; i < arr.length; i++) {
-    answer = [...answer, ...arr[i]]; // 2중 배열 풀어줌
-  }
-
-  return answer;
+    
+    return answer;
 }
