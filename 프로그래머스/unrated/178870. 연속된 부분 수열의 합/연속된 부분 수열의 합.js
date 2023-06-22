47120b6076860526ca367e21f64a28ba36f421fb
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2023-06-22 13:53:46 +0900

    [unrated] Title: 연속된 부분 수열의 합, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.js
+++ 프로그래머스/unrated/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.js
@@ -1,29 +1,28 @@
 function solution(sequence, k) {
-    let answer = []; // 정답이 될 수 있는 후보를 담을 배열
-    let sum = 0; 
-    let head = 0; // 포인터
+    let candidate = [];
+    let sum = 0;
+    let head = 0;
+    let answer = [];
     let min = sequence.length;
-    let result = []; // 답
     
-    for(let i = 0; i<sequence.length;i++){
+    for(let i=0;i<sequence.length;i++){
         sum += sequence[i];
-        if(sum > k){
-          // 작아질 때까지 앞에 값을 빼줌
-            while(sum > k){
-                sum -= sequence[head++];
+        if(sum > k)
+            {
+                while(sum > k){
+                    sum -= sequence[head++];
+                }
             }
-        }
-        if(sum===k){ // k와 같다면 후보에 추가
-            answer.push([head,i]);
-        }
+        if(sum === k)
+            candidate.push([head,i]);      
     }
     
-    answer.forEach((el)=>{
+    candidate.map((el) => {
         if(min > el[1] - el[0]){
             min = el[1] - el[0];
-            result = [el[0],el[1]];
+            answer = [...el];
         }
-    }) 
+    })
     
-    return result;
+    return answer;
 }
