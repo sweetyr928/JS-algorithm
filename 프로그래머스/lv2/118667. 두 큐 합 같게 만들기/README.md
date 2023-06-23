5b61c734a6be0ce2960657f2a94be8c7baa2f64e
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2023-06-23 12:25:38 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 17.66 ms, Memory: 45.6 MB -BaekjoonHub

--- 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/README.md
+++ 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 두 큐 합 같게 만들기 - 118667 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118667#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118667) 
 
 ### 성능 요약
 
-메모리: 44.5 MB, 시간: 25.50 ms
+메모리: 45.6 MB, 시간: 17.66 ms
 
 ### 구분
 
--- 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
+++ 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
@@ -1,20 +1,17 @@
-const solution = (queue1, queue2) => {
-  let sum1 = queue1.reduce((prev, cur) => prev + cur, 0);
-  let sum2 = queue2.reduce((prev, cur) => prev + cur, 0);
-  const half = (sum1 + sum2) / 2;
-  const q = [...queue1, ...queue2];
-  let q1Pointer = 0;
-  let q2Pointer = queue1.length;
-
-  for (let cnt = 0; cnt < queue1.length * 3; cnt++) {
-    if (sum1 === half) {
-      return cnt;
+function solution(queue1, queue2) {
+    let newQ = [...queue1,...queue2];
+    let p1 = 0;
+    let p2 = queue1.length;
+    let half = (queue1.reduce((a,c) => a+=c,0) + queue2.reduce((a,c) => a+=c,0)) / 2;
+    let sum1 = queue1.reduce((a,c) => a+=c,0);
+    
+    for(let i=0;i<queue1.length * 3;i++){
+        if(sum1 === half) return i;
+        else{
+            sum1 = sum1 > half ? 
+                sum1 -= newQ[p1++ % newQ.length] : sum1 += newQ[p2++ % newQ.length];
+        }
     }
-    sum1 =
-      sum1 > half
-        ? sum1 - q[q1Pointer++ % q.length]
-        : sum1 + q[q2Pointer++ % q.length];
-  }
-
-  return -1;
-};
+    
+    return -1;
+}
