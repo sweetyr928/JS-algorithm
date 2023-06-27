43a2e74640b4cf614e16ebb3a81e115810decfc5
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2023-06-27 11:51:44 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 18.88 ms, Memory: 40.3 MB -BaekjoonHub

--- 프로그래머스/lv3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.js
+++ 프로그래머스/lv3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.js
@@ -1,27 +1,22 @@
 function solution(gems) {
-    const cnt = new Set(gems).size;
-    var ans = [1, gems.length];
-
-    var l = 0, r = 0;
-    const hit = new Map();
-    hit.set(gems[0], 1)
-
-    while (r < gems.length) {
-        if (hit.size === cnt) {
-            if(ans[1] - ans[0] > r - l)
-                ans = [l + 1, r + 1]
-
-            hit.set(gems[l], hit.get(gems[l]) - 1);
-            if (hit.get(gems[l]) === 0) 
-                hit.delete(gems[l])
-
+    let answer = [1,gems.length];
+    const map = new Map();
+    map.set(gems[0],1);
+    let l=0, r=0;
+    const all = new Set(gems).size;
+    
+    while(r < gems.length){
+        if(map.size === all){
+            if(answer[1] - answer[0] > r-l)
+                answer = [l+1, r+1];
+            map.set(gems[l], map.get(gems[l])-1);
+            if(!map.get(gems[l])) map.delete(gems[l]);
             l++;
-        }
-        else {
+        }else{
             r++;
-            const right = hit.get(gems[r]);
-            hit.set(gems[r], right ? right + 1 : 1);
+            map.set(gems[r],(map.get(gems[r]) || 0)+1);
         }
     }
-    return ans;
+    
+    return answer;
 }
