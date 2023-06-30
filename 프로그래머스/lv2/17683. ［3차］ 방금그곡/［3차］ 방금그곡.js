9906d2517257535b37fc510455b43de44761c3e6
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2023-06-30 13:43:49 +0900

    [level 2] Title: [3차] 방금그곡, Time: 7.43 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/lv2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.js
+++ 프로그래머스/lv2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.js
@@ -1,33 +1,49 @@
 function solution(m, musicinfos) {
-    const arr = musicinfos.map((mi)=>{ // 입력 배열의 문자열 각각에 대해 수행한다.
-        const [start, end, title, code] = mi.split(","); // 문자열에서 ,를 구분자로 각 정보를 분리
-        const hour = end.slice(0,2)-start.slice(0,2); // 문자열의 0~1번째 문자는 시간
-        const minute = end.slice(3)-start.slice(3); // 문자열의 3~4번째 문자는 분
-        const runtime = 60 * hour + minute; // 총 재생 시간
-
-        const codeArr = code.match(/[A-Z]#?/g); // 알파벳, 알파벳#를 찾아 각각 배열의 인덱스에 분리한다.
-        let stream = code.repeat(Math.floor(runtime/codeArr.length)); // 곡이 반복된 수 n만큼 악보 정보를 더한다.
-        stream += codeArr.slice(0,runtime % codeArr.length).join(""); // 재생 도중에 멈춘 시점의 음까지를 추가로 더해준다.
-        return [title, runtime, stream]; // 음악 제목, 총 재생 시간, 재생된 음 정보를 반환한다.
-    })
-
-    // 다음의 filter 함수는 조건을 충족하는(return true하는) 원소만 출력하는 함수
-    const answer = arr.filter(([_,__,stream])=>{ // 각각의 결과 배열에 대해 다음을 수행한다.
-        let i = stream.indexOf(m); // 재생된 음 정보에 문자열 m이 포함되었는지 검사한다.
-        if(i === -1) return false; // 포함되어있지 않다면 넘어간다.
-        while(i !== -1){ // 포함되어 있다면
-            if(stream[i+m.length] !== "#") return true;
-            i = stream.indexOf(m, i+1);
-           // 찾은 문자 뒤에 #이 없다면 배열 정보를 그대로 반환한다.
-           // ( stream = ABC#EFG 일 때 m= ABC를 찾을 수 있다.) 
-           // 찾은 문자 뒤에 #이 있다면 정답이 아니므로, 찾은 문자 뒤에 m과 일치하는 문자가 있는지 찾아야 한다.
-        }        
-    });
-    if(!answer.length) return "(None)"; // answer 배열에 원소가 없다면 조건에 일치하는 음악이 없는 것.
-
-    answer.sort((a,b)=>{
-        if(a[1] === b[1]) return 0;
-        return b[1] - a[1];
-    }) // 1번 인덱스(총 재생수)가 같다면 순서를 유지하고, 같지 않다면 내림차순 정렬한다.
-    return answer[0][0]; // 입력 순서 + 내림차순을 반영했으므로 배열의 첫번째 원소의 제목이 정답이다.
+    let answer = "(None)";
+    let longestTime = 0;
+    
+    const getMinuate = (time) => {
+        const [hh,mm] = time.split(":");
+        return +hh * 60 + +mm;
+    }
+    
+    const getArray = (str) => {
+        let arr = [];
+        for(let i=0;i<str.length;i++){
+            let temp = str[i];
+            if(str[i] === "#") continue;
+            if(str[i+1] === "#") temp += str[i+1];
+            arr.push(temp);
+        }
+        
+        return arr;
+    }
+    
+    const checkMelody = (all, part) => {
+        if(all.indexOf(part) !== -1){
+            let next = all.indexOf(part) + part.length;
+            
+            if(all[next] === "#") return checkMelody(all.slice(next+1),part);
+            
+            return true;
+        }
+        
+        return false;
+    }
+    
+    for(let i=0;i<musicinfos.length;i++){
+        const [start,end,name,code] = musicinfos[i].split(",");
+        const tempTime = getMinuate(end) - getMinuate(start);
+        let allMelody = "";
+        let codeToArr = getArray(code)
+        for(let j=0;j<tempTime;j++)
+            allMelody += codeToArr[j%codeToArr.length];
+        
+        if(checkMelody(allMelody,m) && longestTime < tempTime){
+            answer = name;
+            longestTime = tempTime;
+        }
+    }
+    
+    return answer;
 }
