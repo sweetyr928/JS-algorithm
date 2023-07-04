9b928140aeee9c8c3381fbd4b1d0b84b0442032b
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2023-07-04 11:26:07 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 1.01 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.js
+++ 프로그래머스/lv2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.js
@@ -7,17 +7,18 @@ function solution(expression) {
         ['+', '-', '*'],
         ['+', '*', '-']
     ];
-    let cand = [];
-
-    for (let opCand of prior) {
-        const temp = expression.split(/(\D)/);
-        for (let exp of opCand) {
-            while (temp.includes(exp)) {
-                const idx = temp.indexOf(exp)
-                temp.splice(idx - 1, 3, eval(temp.slice(idx - 1, idx + 2).join('')));
+    let answer = [];
+    
+    for(let arr of prior){
+        let temp = expression.split(/(\D)/);
+        for(let exp of arr){
+            while(temp.includes(exp)){
+                let idx = temp.indexOf(exp);
+                temp.splice(idx-1,3,eval(temp.slice(idx-1,idx+2).join("")));
             }
         }
-        cand.push(Math.abs(temp[0]));
+        answer.push(Math.abs(temp[0]));
     }
-    return Math.max(...cand);
+    
+    return Math.max(...answer);
 }
