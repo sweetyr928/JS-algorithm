4100174760d27852b798057621aa6ef7ea3fed2a
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2023-11-28 19:02:21 +0900

    [level 2] Title: 구명보트, Time: 10.69 ms, Memory: 38.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -0,0 +1,14 @@
+function solution(people, limit) {
+    let sortedList = people.sort((a,b) => a-b)
+    let answer = 0
+    
+    while(sortedList.length){
+        if(sortedList[0] + sortedList[sortedList.length-1] <= limit){
+            sortedList.shift()
+        }
+        sortedList.pop()
+        answer++
+    }
+    
+    return answer
+}
