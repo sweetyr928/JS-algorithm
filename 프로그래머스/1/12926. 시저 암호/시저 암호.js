cec1feea9f3f20e83b98724fb76079b24d3b99cd
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2023-11-30 19:13:25 +0900

    [level 1] Title: 시저 암호, Time: 5.51 ms, Memory: 37.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.js
@@ -0,0 +1,18 @@
+function solution(s, n) {
+    const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const lower = "abcdefghijklmnopqrstuvwxyz";
+    let answer= '';
+
+    for(let i = 0;i<s.length;i++){
+        if(s[i] === ' ') {
+            answer += ' '; 
+        }else{
+            let arr = upper.includes(s[i]) ? upper : lower;
+            let idx = arr.indexOf(s[i]) + n;
+            if(idx >= arr.length) idx -= arr.length;
+            answer += arr[idx];   
+        }
+    }
+    
+    return answer;
+}
