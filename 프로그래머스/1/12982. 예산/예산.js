9e05fa66d0aa2311339465050ad0985f1d86e01e
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2023-12-02 09:18:02 +0900

    [level 1] Title: 예산, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -0,0 +1,13 @@
+function solution(d, budget) {
+    let answer = 0;
+    d = d.sort((a,b) => a-b)
+    
+    for(let i=0;i<d.length;i++){
+        if(budget - d[i] >= 0){
+           budget -= d[i]
+           answer++ 
+        }else break
+    }
+    
+    return answer;
+}
