2a0998bfd60b751901c7802aae807b65f331ca15
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2023-12-07 18:45:02 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 16.38 ms, Memory: 41.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -0,0 +1,16 @@
+function solution(s) {
+    let answer = [];
+    let obj = {}
+    
+    for(let i=0;i<s.length;i++){
+        if(!obj[s[i]]){
+            obj[s[i]] = [i]
+            answer.push(-1)
+        }else{
+            answer.push(i-obj[s[i]][obj[s[i]].length-1])
+            obj[s[i]] = [...obj[s[i]],i]
+        }
+    }
+    
+    return answer;
+}
