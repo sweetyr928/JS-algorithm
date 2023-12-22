49f4561841d9fb57f6a6aec645f1c39c57e717d7
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2023-12-22 19:09:30 +0900

    [level 3] Title: 야근 지수, Time: 26.21 ms, Memory: 36.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.js
@@ -0,0 +1,18 @@
+function solution(n, works) {
+    if(works.reduce((a,c)=>a+=c) <= n) return 0
+    let answer = 0
+    let sorted = works.sort((a,b) => a-b)
+  
+    while(n) {
+        const max = sorted[works.length-1]
+        for(let i = works.length-1; i >= 0; i--) {
+            if(sorted[i] >= max) {
+                sorted[i]--
+                n--
+            }
+            if(!n) break
+        }
+    }
+
+    return works.reduce((a,c) => a + Math.pow(c, 2), 0)
+}
