d415f381c6217caa0ca23d5f45c095e0da9d3235
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2023-12-27 17:45:13 +0900

    [level 1] Title: 폰켓몬, Time: 4.89 ms, Memory: 37.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -0,0 +1,9 @@
+function solution(nums) {
+    let map = new Map()
+    
+    for(let i=0;i<nums.length;i++){
+        map.set(nums[i], map.get(nums[i] || 0) + 1)
+    }
+    
+    return map.size > nums.length / 2 ? nums.length / 2 : map.size
+}
