5ce74dce7fdfe4b8bcea8b78eb980560ea21d81e
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2023-12-28 22:15:30 +0900

    [level 2] Title: 프로세스, Time: 0.44 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -0,0 +1,15 @@
+function solution(priorities, location) {
+    let work = priorities.map((el,idx) => [el,idx])
+    let queue = []
+    
+    while(work.length){
+        let cur = work.shift()
+        if(cur[0] >= Math.max(...work.map((el)=>el[0]))){
+            queue.push(cur[1])
+            
+            if(cur[1] === location) return queue.indexOf(cur[1])+1
+        }else{
+            work.push(cur)
+        }
+    }
+}
