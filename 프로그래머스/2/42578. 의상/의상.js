e027f4633c1569834544829690615b10732ea822
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2023-12-30 23:52:36 +0900

    [level 2] Title: 의상, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -0,0 +1,13 @@
+function solution(clothes) {
+    let map = new Map()
+    for(let i=0;i<clothes.length;i++){
+        map.set(clothes[i][1], (map.get(clothes[i][1]) || 0) + 1)
+    }
+     let answer = 1
+     
+     for(let value of map.values()){
+        answer *= (1 + value)
+    }
+    
+    return answer - 1
+}
