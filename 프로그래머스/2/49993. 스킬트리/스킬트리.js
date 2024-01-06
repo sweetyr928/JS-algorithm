8fda527f0bf43b0d5d6c901d1943453f43ccf224
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2024-01-06 13:53:54 +0900

    [level 2] Title: 스킬트리, Time: 0.21 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.js
@@ -0,0 +1,20 @@
+function solution(skill, skill_trees) {
+    let newSkill = {}
+    for(let i=0;i<skill.length;i++) newSkill[skill[i]] = i
+    let answer = 0
+    
+    for(let i=0;i<skill_trees.length;i++){
+        let temp = skill_trees[i].split("").filter(el => skill.includes(el))
+        let temp2 = temp.slice().filter((el,idx) => el === skill[idx])
+        temp.length === temp2.length ? answer++ : null
+    }
+    
+    return answer
+}
+
+// "CBD", ["CAD"] 0
+// "CBD", ["AEF", "ZJW"] 2
+// "REA", ["REA", "POA"] 1
+// "CBDK", ["CB", "CXYB", "BD", "AECD", "ABC", "AEX", "CDB", "CBKD", "IJCB", "LMDK"] 4
+// "BDC", ["AAAABACA"] 0
+// "CBD", ["C", "D", "CB", "BDA"] 2
