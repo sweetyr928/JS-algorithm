64cfdf05a5e022a4783adff295036799db91b45c
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2024-01-09 17:36:28 +0900

    [level 2] Title: 삼각 달팽이, Time: 2486.24 ms, Memory: 112 MB -BaekjoonHub

---
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.js
@@ -0,0 +1,33 @@
+function solution(n) {
+    let answer = []
+    let arr = Array.from({length : n}, (_,idx) => new Array(idx+1).fill(0))
+    let x = 0 // 좌우
+    let y = -1 // 상하
+    let cur = 0
+    
+    while(n > 0){
+        for(let i=0;i<n;i++){ // 위에서 아래
+            y++
+            cur++
+            arr[y][x] = cur
+        }
+        for(let i=0;i<n-1;i++){ // 왼에서 오
+            x++
+            cur++
+            arr[y][x] = cur
+        }
+        for(let i=0;i<n-2;i++){ // 아래에서 위
+            x--
+            y--
+            cur++
+            arr[y][x] = cur
+        }
+        n -= 3
+    }
+    
+    for(let i=0;i<arr.length;i++){
+        answer = [...answer,...arr[i]]
+    }
+    
+    return answer
+}
