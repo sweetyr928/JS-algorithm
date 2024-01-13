720ce6378a8f595225cbcf12dbe3d53e079885b2
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2024-01-13 22:12:07 +0900

    [level Lv.3] Title: [카카오 인턴] 보석 쇼핑, Time: 17.06 ms, Memory: 40.3 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.js
@@ -0,0 +1,22 @@
+function solution(gems) {
+    let answer = [1,gems.length]
+    const map = new Map()
+    map.set(gems[0],1)
+    let l=0, r=0
+    const all = new Set(gems).size
+    
+    while(r < gems.length){
+        if(map.size === all){
+            if(answer[1] - answer[0] > r-l)
+                answer = [l+1, r+1]
+            map.set(gems[l], map.get(gems[l])-1)
+            if(!map.get(gems[l])) map.delete(gems[l])
+            l++
+        }else{
+            r++
+            map.set(gems[r],(map.get(gems[r]) || 0)+1)
+        }
+    }
+    
+    return answer
+}
