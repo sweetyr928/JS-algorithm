3050ea06a3b10643cdec17c39f0e1248cb050ff7
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2024-01-16 20:34:02 +0900

    [level Lv.3] Title: 이중우선순위큐, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.3/42628. 이중우선순위큐/이중우선순위큐.js
@@ -0,0 +1,20 @@
+function solution(operations) {
+    let temp = []
+    
+    for(let i=0;i<operations.length;i++){
+        let [o,n] = operations[i].split(" ")
+        if(o === "I") temp.push(Number(n))
+        if(o === "D" && n === "1"){
+            let max = Math.max(...temp)
+            temp.splice(temp.indexOf(max),1)
+        }
+        if(o === "D" && n === "-1"){
+            let min = Math.min(...temp)
+            temp.splice(temp.indexOf(min),1)
+        }
+    }
+    
+    if (temp.length === 0) return [0,0]
+    
+    return [Math.max(...temp),Math.min(...temp)]
+}
