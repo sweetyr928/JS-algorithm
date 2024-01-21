633ffd08fdaee91fc9ada7f1b1e4a24e9e1b8b57
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2024-01-21 23:51:50 +0900

    [level 2] Title: 무인도 여행, Time: 0.37 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.js
@@ -0,0 +1,34 @@
+function solution(maps) {
+    const newM = maps.map((el) => el.split(""))
+    const dx = [1,0,-1,0]
+    const dy = [0,1,0,-1]
+    let answer = []
+    
+    const dfs = (x,y,cnt) => {
+        let total = +cnt
+        
+        for(let i=0;i<4;i++){
+            if(x+dx[i] >= 0 && x+dx[i] < maps.length && y+dy[i] >= 0 && y+dy[i] < maps[0].length){
+                if(newM[x+dx[i]][y+dy[i]] !== "X"){
+                    let start = newM[x+dx[i]][y+dy[i]]
+                    newM[x+dx[i]][y+dy[i]] = "X"
+                    total += dfs(x+dx[i],y+dy[i],start)
+                }
+             }
+        }
+        
+        return total
+    }
+    
+    for(let i=0;i<newM.length;i++){
+        for(let j=0;j<newM[0].length;j++){
+            if(newM[i][j] !== "X"){
+                let start = +newM[i][j]
+                newM[i][j] = 'X'
+                answer.push(dfs(i,j,start))
+            }
+        }
+    }
+    
+    return answer.length ? answer.sort((a,b)=>a-b):[-1]
+}
