cef479e76f3958ba7c9efd4b3daf09653902bf3a
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2024-01-23 22:10:47 +0900

    [level 3] Title: 숫자 게임, Time: 85.85 ms, Memory: 44.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.js
@@ -0,0 +1,14 @@
+function solution(A, B) {
+    let answer = 0, j = 0
+    A.sort((a,b) => b-a)
+    B.sort((a,b) => b-a)
+    
+    for(let i=0;i<A.length;i++){
+        if(A[i] < B[j]){
+            answer++
+            j++
+        }
+    }
+    
+    return answer
+}
