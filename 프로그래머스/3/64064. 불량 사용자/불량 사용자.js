b8e67290787762ea7dd8d7e0f9cb1c7cc9db0422
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2024-01-26 17:37:24 +0900

    [level 3] Title: 불량 사용자, Time: 0.42 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.js
@@ -0,0 +1,33 @@
+function solution(user_id, banned_id) {
+    const ch = new Array(user_id.length).fill(0)
+    const set = new Set()
+    
+    const isPossible = (uId, bId) => {
+        if (uId.length !== bId.length) return false
+        for (let i = 0; i < uId.length; i++) {
+            if (bId[i] !== '*' && uId[i] !== bId[i]) return false
+        }
+        return true
+    }
+    
+    const check = (idx, cnt, possible) => {
+        if (cnt === banned_id.length) 
+            set.add(possible.split(' ').sort().join(''))
+        if (idx >= banned_id.length) return
+        
+        for (let i = idx; i < banned_id.length; i++) {
+            for (let j = 0; j < user_id.length; j++) {
+                if (ch[j] === 1) continue
+                if (!isPossible(user_id[j], banned_id[i])) continue
+                
+                ch[j] = 1
+                check(i+1, cnt+1, possible + ' ' + user_id[j])
+                ch[j] = 0
+            }
+        }
+    }
+    
+    check(0, 0, '')
+    
+    return set.size
+}
