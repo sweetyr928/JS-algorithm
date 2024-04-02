2b30ec3f8c976436322af229e29366cadfbfa670
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2024-04-02 23:25:55 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -2,9 +2,7 @@ function solution(phone_number) {
     let behindFour = phone_number.slice(-4)
     let star = ''
     
-    for(let i=0;i<phone_number.length - behindFour.length;i++){
-        star += '*'
-    }
+    for(let i = 0;i < phone_number.length - behindFour.length;i++) star += '*'
     
     return star + behindFour
 }
