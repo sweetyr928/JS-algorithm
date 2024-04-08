63d896bdcd2f3dc13dfb120b3506cb85164ce586
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2024-04-08 21:47:53 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 12.48 ms, Memory: 83.3 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,9 +1,9 @@
-function solution(arr)
-{
+function solution(arr){
     let answer = []
     
-    for(let i=0;i<arr.length;i++){
-        if(arr[i] !== answer[answer.length-1]) answer.push(arr[i])
+    for(let i = 0;i < arr.length;i++){
+        if(arr[i] !== answer[answer.length-1]) 
+            answer.push(arr[i])
     }
     
     return answer
