444292daefabbadcb230fb229a8d32ca0f57dad1
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2024-04-15 21:05:51 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -1,8 +1,8 @@
 function solution(n) {
-    let dp=[0,1,2]
+    let dp = [0,1,2]
 
-    for(let i=3;i<=n;i++){
-        dp[i] = (dp[i-1]+dp[i-2]) % 1234567
+    for(let i = 3;i <= n;i++){
+        dp[i] = (dp[i-1] + dp[i-2]) % 1234567
     }
 
     return dp[n] % 1234567
