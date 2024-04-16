8934c7a70f96380218c44ec263079dd2909a3919
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2024-04-16 23:56:54 +0900

    [level 2] Title: 귤 고르기, Time: 48.71 ms, Memory: 52.7 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
@@ -1,17 +1,15 @@
 function solution(k, tangerine) {
     let map = new Map()
+    let temp = 0, i = 0
     
     for(let i=0;i<tangerine.length;i++){
         map.set(tangerine[i], (map.get(tangerine[i]) || 0) + 1)
     }
-    
+
     const arr = [...map].sort((a,b) => b[1]-a[1])
-    
-    let temp = 0, i = 0
     
     while(temp < k){
       if(arr[i][1] >= k) return i+1
-        
       if(temp < k){
         temp += arr[i][1]
         i++
