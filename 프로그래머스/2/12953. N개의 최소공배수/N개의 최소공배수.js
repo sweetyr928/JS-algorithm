76ab616ec447875c9157e86242853a3366c013be
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2024-04-22 21:11:40 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.17 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
@@ -1,7 +1,7 @@
 function solution(arr) {
-  return arr.reduce((a, b) => (a * b) / getGcd(a, b));
+  return arr.reduce((a, b) => (a * b) / getGcd(a, b))
 }
 
 function getGcd(a, b) {
-  return a % b ? getGcd(b, a % b) : b;
+  return a % b ? getGcd(b, a % b) : b
 }
