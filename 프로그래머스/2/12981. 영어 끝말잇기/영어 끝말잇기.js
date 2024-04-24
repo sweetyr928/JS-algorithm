34e05fc7cfd5838f6b35542d688f2359364d7bdd
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2024-04-24 18:09:40 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,11 +1,11 @@
 function solution(n, words) {
-    let alreadyDone = [words[0]]
+    let done = [words[0]]
     
     for(let i = 1;i < words.length;i++){
         if(words[i][0] !== words[i-1][words[i-1].length-1] ||
-          alreadyDone.includes(words[i]))
-            return [(i+1)%n > 0 ? (i+1) % n : n, Math.ceil((i+1)/n)]
-        else alreadyDone.push(words[i])
+          done.includes(words[i]))
+            return [(i + 1) % n > 0 ? (i + 1) % n : n, Math.ceil((i + 1) / n)]
+        else done.push(words[i])
     }
     
     return [0,0]
