a217ead813875abb3c37e24592c4b1790c806549
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2024-05-03 23:25:24 +0900

    [level 2] Title: 기능개발, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -0,0 +1,20 @@
+function solution(progresses, speeds) {
+    let answer = []
+    let time = progresses.map((el,idx) => Math.ceil((100-el)/speeds[idx]))
+    let max = time[0]
+    let cnt = 0
+    
+    time.map((el) => {
+        if(max >= el){
+            cnt++
+        }else{
+            answer.push(cnt)
+            cnt = 1
+            max = el
+        }
+    })
+    
+    answer.push(cnt)
+    
+    return answer
+}
