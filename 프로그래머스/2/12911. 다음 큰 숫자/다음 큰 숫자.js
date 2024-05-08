a91ad09dc2db1cb35af0b4ad904d0b12dff56476
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2024-05-08 19:39:36 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -1,7 +1,9 @@
 function solution(n) {
     let nextNum = n + 1
+    
     while(true){
-        if(n.toString(2).split('').filter(el=> el>0).length === nextNum.toString(2).split('').filter(el=> el>0).length) break
+        if(n.toString(2).split('').filter(el=> el>0).length 
+           === nextNum.toString(2).split('').filter(el=> el>0).length) break
         nextNum += 1
     }
     
