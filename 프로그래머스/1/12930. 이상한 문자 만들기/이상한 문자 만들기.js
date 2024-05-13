4b34fb0a39e6f988af183222bd85e1964aa44884
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2024-05-13 22:37:08 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.26 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -0,0 +1,22 @@
+function solution(s) {
+    var answer = ''
+    let arr = s.split(' ')
+    let temp = ''
+    
+    for(let i = 0;i < arr.length;i++){
+        if(arr[i].length > 0){
+            temp = ''
+            for(let j = 0;j < arr[i].length;j++){
+                if(j % 2===0){
+                    temp += arr[i][j].toUpperCase()
+                }else{
+                    temp += arr[i][j].toLowerCase()
+                }
+            }
+            arr[i] = temp
+        }
+    }
+    answer = arr.join(' ')
+    
+    return answer
+}
