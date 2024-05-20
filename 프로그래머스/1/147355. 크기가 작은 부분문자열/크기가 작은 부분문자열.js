32e379723df2aba2be78aa07a3d925d32c7ff35e
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2024-05-20 19:21:52 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -2,12 +2,12 @@ function solution(t, p) {
     let answer = 0
     let pLength = p.length
     
-    for(let i=0;i<=t.length-pLength;i++){
+    for(let i = 0;i <= t.length - pLength;i++){
         let str = ''
         for(let j=0;j<pLength;j++)
             str += t[i+j]
         if(str <= p) answer++
     }
     
-    return answer;
+    return answer
 }
