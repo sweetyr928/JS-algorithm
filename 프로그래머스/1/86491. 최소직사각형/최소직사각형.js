c35b0301aa7f845657444694a9e53574cc3668ff
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2024-05-21 21:56:17 +0900

    [level 1] Title: 최소직사각형, Time: 21.79 ms, Memory: 39.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -0,0 +1,10 @@
+function solution(sizes) {
+    for(let i = 0;i < sizes.length;i++){
+        if(sizes[i][0] < sizes[i][1]) [sizes[i][0],sizes[i][1]] = [sizes[i][1],sizes[i][0]]
+    }
+    
+    let temp_width = sizes.slice().sort((a,b) => b[0] - a[0])
+    let temp_height = sizes.slice().sort((a,b) => b[1] - a[1])
+    
+    return temp_width[0][0] * temp_height[0][1]
+}
