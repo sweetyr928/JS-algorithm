ba605ef191378702175741348fd9ac1d30e48514
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2024-05-27 22:28:18 +0900

    [level 2] Title: 행렬의 곱셈, Time: 3.31 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
@@ -2,10 +2,10 @@ function solution(arr1, arr2) {
     let answer = []
     let temp = new Array(arr2[0].length).fill(0)
     
-    for(let i=0;i<arr1.length;i++){
+    for(let i = 0;i < arr1.length;i++){
         temp = new Array(arr2[0].length).fill(0)
-        for(let j=0;j<arr2[0].length;j++){
-            for(let k=0;k<arr2.length;k++){
+        for(let j = 0;j < arr2[0].length;j++){
+            for(let k = 0;k < arr2.length;k++){
                 temp[j] += arr1[i][k] * arr2[k][j]
             }
         }
