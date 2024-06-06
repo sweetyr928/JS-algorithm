ae6b451c0fed181b5eefe24d1448fddd144c6838
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2024-06-06 23:23:21 +0900

    [level 2] Title: 모음 사전, Time: 2.15 ms, Memory: 35.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.js
@@ -0,0 +1,19 @@
+function solution(word) {
+    let dic = []
+    let alpha = ['A', 'E', 'I', 'O', 'U']
+    
+    const dfs = (fixed, len) => {
+        if(fixed.length === len){
+            dic.push(fixed)
+            return
+        }
+        for(let i=0;i<alpha.length;i++){
+            let newFixed = fixed + alpha[i]
+            dfs(newFixed,len)
+        }
+    }
+        
+    for(let i=1;i<=5;i++) dfs("",i)
+            
+    return dic.sort().indexOf(word) + 1
+}
