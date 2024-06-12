3348c48b66a42fb3df0dac8c8a6d84f7b7d2e7c4
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2024-06-12 19:15:47 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 1170.63 ms, Memory: 62.3 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -1,10 +1,10 @@
 function solution(elements) {
     let answer = new Set()
     
-    for(let i=0;i<elements.length;i++){
+    for(let i = 0;i < elements.length;i++){
         let els = elements.concat(elements.slice(0,i))
         
-        for(let j=0;j<elements.length;j++){
+        for(let j = 0;j < elements.length;j++){
             answer.add(els.slice(j,j+i+1).reduce((a,c) => a+=c),0)
         }
     }
