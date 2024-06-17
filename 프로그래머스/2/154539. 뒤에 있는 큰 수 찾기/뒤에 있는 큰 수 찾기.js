fc6f39593da2c54015af6bda289479a1d34f9574
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2024-06-17 19:00:40 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 46.98 ms, Memory: 149 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.js
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.js
@@ -2,7 +2,7 @@ function solution(numbers) {
     const answer = new Array(numbers.length).fill(-1)
     const stack = []
     
-    for(let i=0;i<numbers.length;i++){
+    for(let i = 0;i < numbers.length;i++){
         while(stack.length && numbers[stack[stack.length-1]] < numbers[i])
             answer[stack.pop()] = numbers[i]
         stack.push(i)
