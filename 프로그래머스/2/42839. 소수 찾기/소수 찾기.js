306c5b11df9e757015467d50e4ffc87c1c601e0b
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2024-06-19 22:33:02 +0900

    [level 2] Title: 소수 찾기, Time: 0.40 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
@@ -0,0 +1,29 @@
+function solution(numbers) {
+    let answer = []
+    numbers = numbers.split("")
+    
+    const isRight = (n) => {
+        if(n<=1) return false
+        for(let i=2;i<=Math.sqrt(n);i++){
+            if(n%i === 0) return false
+        }
+        return true
+    }
+    
+    const dfs = (left,fixed) => {
+        if(left.length > 0){
+            for(let i=0;i<left.length;i++){
+                let temp = fixed + left[i]
+                let newLeft = left.slice()
+                newLeft.splice(i,1)
+                if(isRight(+temp) && !answer.includes(+temp))
+                    answer.push(+temp)
+                dfs(newLeft,temp)
+            }
+        }
+    }
+    
+    dfs(numbers,"")
+    
+    return answer.length
+}
