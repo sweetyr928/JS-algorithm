a952fe4f1a430a0e43748ee8ed23e5548a8e8d75
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2024-06-21 20:39:02 +0900

    [level 2] Title: 구명보트, Time: 10.53 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.js
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -1,9 +1,9 @@
 function solution(people, limit) {
-    let sortedList = people.sort((a,b) => a-b)
+    let sortedList = people.sort((a, b) => a - b)
     let answer = 0
     
     while(sortedList.length){
-        if(sortedList[0] + sortedList[sortedList.length-1] <= limit){
+        if(sortedList[0] + sortedList[sortedList.length - 1] <= limit){
             sortedList.shift()
         }
         sortedList.pop()
