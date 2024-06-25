d5bf0285c2cc8441f19feff9a8e951d5bd7ea28c
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2024-06-26 08:41:14 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 285.42 ms, Memory: 57.7 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.js
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.js
@@ -1,13 +1,13 @@
 function solution(numbers) {
     let answer = []
     
-    for(let i=0;i<numbers.length;i++){
+    for(let i = 0;i < numbers.length;i++){
         if(numbers[i] % 2 === 0) answer.push(numbers[i]+1)
         else{
             let temp = numbers[i].toString(2).split("")
             let zero = false
             
-            for(let j=temp.length-1;j>=0;j--){
+            for(let j = temp.length - 1;j >= 0;j--){
                 if(temp[j] === '0'){
                     temp[j] = 1
                     temp[j+1] = 0    
