7cdcb3069e9658b1d54b552d3a230f7742f8ae6b
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2024-06-27 18:27:57 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.js
@@ -0,0 +1,19 @@
+function solution(n, k) {
+    let answer = 0
+    let newN = n.toString(k)
+    newN = newN.split("0").filter(el => el !== " " && el > 1)
+    let prime = true
+    
+    for(let i=0;i<newN.length;i++){
+        prime = true
+        for(let j=2;j<=Math.sqrt(newN[i]);j++){
+            if(newN[i] % j === 0) {
+                prime = false
+                break
+            }
+        }
+        if(prime) answer++
+    }
+    
+    return answer
+}
