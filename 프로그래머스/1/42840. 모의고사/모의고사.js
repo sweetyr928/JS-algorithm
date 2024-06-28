96a0be364ddc654df7678cdec3b3327e18cf7395
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2024-06-28 18:46:49 +0900

    [level 1] Title: 모의고사, Time: 3.49 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -5,16 +5,16 @@ function solution(answers) {
     let count = new Array(3).fill(0)
     let ans = []
     
-    for(let i=0;i<answers.length;i++){
+    for(let i = 0;i < answers.length;i++){
         if(answers[i] === one[i % one.length]) count[0]++
         if(answers[i] === two[i % two.length]) count[1]++
         if(answers[i] === three[i % three.length]) count[2]++
     }
     let max = Math.max(...count)
     
-    for(let i=0;i<count.length;i++){
+    for(let i = 0;i < count.length;i++){
         if(count[i] === max) ans.push(i+1)
     }
     
-    return ans;
+    return ans
 }
