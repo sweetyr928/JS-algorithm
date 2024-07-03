db6afcac8d61b6ad45369c0d030a5401c75bbc94
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2024-07-03 16:03:32 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.72 ms, Memory: 33.1 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -1,7 +1,7 @@
 function solution(A,B){
     const sortedA = A.sort((a,b) => a - b)
     const sortedB = B.sort((a,b) => b - a)
-    let sum = 0;
+    let sum = 0
     
     for(let i = 0;i < A.length;i++)
         sum += sortedA[i] * sortedB[i]
