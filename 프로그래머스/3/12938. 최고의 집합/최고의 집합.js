3cc8985c840a049a8c04826b7a27a5c8a55d1d79
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2024-07-19 13:56:09 +0900

    [level 3] Title: 최고의 집합, Time: 0.39 ms, Memory: 37.3 MB -BaekjoonHub

--- 프로그래머스/3/12938. 최고의 집합/최고의 집합.js
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.js
@@ -1,9 +1,10 @@
 function solution(n, s) {
     if(n > s) return [-1]
 
-    const answer = new Array(n).fill(Math.floor(s / n))
+    const ans = new Array(n).fill(Math.floor(s / n))
+    
     for(let i = 0; i < s % n; i ++)
-        answer[answer.length - 1 - i]++
+        ans[ans.length - 1 - i]++
   
-    return answer
+    return ans
 }
