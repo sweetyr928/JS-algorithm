fb2f096a648a956b2ec0b5e4e726e9f290e8548f
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2024-07-23 20:55:45 +0900

    [level 2] Title: 괄호 회전하기, Time: 28.22 ms, Memory: 39.1 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -17,8 +17,10 @@ function isRight(str){
     if(str[0] === ")" || str[0] === "]" || str[0] === "}") return false
     
     for(let i = 0;i < str.length;i++){
-        if(str[i] === "(" || str[i] === "[" || str[i] === "{") stack.push(str[i])
-        else if(str[i]===")" && stack[stack.length-1]==="(" || str[i]==="}" && stack[stack.length-1]==="{" || str[i]==="]" && stack[stack.length-1]==="[") stack.pop()
+        if(str[i] === "(" || str[i] === "[" || str[i] === "{") 
+            stack.push(str[i])
+        else if(str[i]===")" && stack[stack.length-1]==="(" || str[i]==="}" && stack[stack.length-1]==="{" || str[i]==="]" && stack[stack.length-1]==="[") 
+            stack.pop()
     }
     
     return stack.length === 0 ? true : false
