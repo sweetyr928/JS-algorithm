7add8b53fc3fe1641800ab11ecbfe9e6d59134a8
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2024-08-22 21:15:35 +0900

    [level 2] Title: 방문 길이, Time: 0.49 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.js
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.js
@@ -3,7 +3,7 @@ function solution(dirs) {
     let now = [0,0]
     let move = {"U" : [0,1], "D" : [0,-1], "L" : [-1,0], "R" : [1,0]}
     
-    for(let i=0;i<dirs.length;i++){
+    for(let i =0;i<dirs.length;i++){
         let nowX = now[0] + move[dirs[i]][0]
         let nowY = now[1] + move[dirs[i]][1]
         if(nowX <= 5 && nowX >= -5 && nowY <= 5 && nowY >= -5){
