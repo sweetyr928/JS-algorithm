828b7dd3a1febbadb48e29d39ca3aa442640aa11
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2024-09-18 11:04:14 +0900

    [level 2] Title: 기능개발, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,6 +1,6 @@
 function solution(progresses, speeds) {
     let answer = []
-    let time = progresses.map((el,idx) => Math.ceil((100-el)/speeds[idx]))
+    let time = progresses.map((el,idx) => Math.ceil((100 -el)/speeds[idx]))
     let max = time[0]
     let cnt = 0
     
