ee3db4deb217a904dedb21ffa696c021c4bf8a37
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2024-10-05 20:14:45 +0900

    [level 1] Title: 소수 찾기, Time: 149.22 ms, Memory: 36.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
@@ -0,0 +1,20 @@
+function solution(n) {
+    var answer = 1;
+    if (n == 2){
+        return answer;
+    }
+    for (let i = 3;i <= n;i+=2){
+        let isPrime = true;
+        for (let j = 3;j <= Math.sqrt(i); j+=2){
+            if(i % j == 0){
+              	isPrime = false;
+                break;
+            }
+        }
+        
+      if(isPrime){
+        answer++;
+      }
+    }
+    return answer;
+}
