10920b3b8d74d88ff0e1b4fe900831bb4fa469d3
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2024-10-08 21:39:33 +0900

    [level 3] Title: 입국심사, Time: 136.76 ms, Memory: 42.4 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.js
+++ 프로그래머스/3/43238. 입국심사/입국심사.js
@@ -5,6 +5,7 @@ function solution(n, times) {
         const mid = Math.floor((low + high) / 2)
         const people = times.reduce((acc, time) => 
             acc + Math.floor(mid / time), 0)
+        
         if (people < n) {
             low = mid + 1
         } else {
