8f72e972ab67ac762f9fb923398d8b855e522bf2
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2024-10-15 23:56:42 +0900

    [level 2] Title: 124 나라의 숫자, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    let ans = ''
+    let arr = ['4','1','2']
+    
+    while(n >0){
+        ans = arr[n%3] + ans
+        
+        n = Math.ceil(n/3-1)
+    }
+    
+    return ans
+}
