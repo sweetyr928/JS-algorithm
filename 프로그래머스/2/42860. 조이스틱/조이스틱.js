3c8c30f6b22ed8e37bbbba2b0d9b3bdeb64053d5
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2024-11-06 23:24:03 +0900

    [level 2] Title: 조이스틱, Time: 0.27 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.js
@@ -0,0 +1,19 @@
+function solution(name) {
+    let tb = 0
+    let lr = name.length - 1;
+    
+    [...name].map((n, i) =>{
+        tb += Math.min(n.charCodeAt() - 65, 91 - n.charCodeAt());
+        let idx = i + 1;
+        
+        while(idx < name.length && name[idx] === "A") idx++;
+        
+        lr = Math.min(
+            lr,
+            i * 2 + name.length - idx,
+            i + 2 * (name.length - idx)
+        )
+    })
+    
+    return tb + lr;
+}
