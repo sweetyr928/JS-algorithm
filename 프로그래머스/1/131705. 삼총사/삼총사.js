a9cfd606a76d9001c154f1ff74543dc909be0c4f
Yerin Jeon <39157466+sweetyr928@users.noreply.github.com>
2024-12-02 23:52:13 +0900

    [level 1] Title: 삼총사, Time: 0.14 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.js
+++ 프로그래머스/1/131705. 삼총사/삼총사.js
@@ -4,9 +4,8 @@ function solution(number) {
     for (let i = 0; i < number.length; i++) {
         for (let j = i + 1; j < number.length; j++) {
             for (let k = j + 1; k < number.length; k++) {
-                if (number[i] + number[j] + number[k] === 0) {
+                if (number[i] + number[j] + number[k] === 0) 
                     count++;
-                }
             }
         }
     }
